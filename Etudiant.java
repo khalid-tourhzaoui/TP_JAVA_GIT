ca7f5b34f539162aebc4e7c12ccdc77adf26f766
khalid-tourhzaoui <tarikkhalidoss100v3356@gmail.com>
2024-02-21 17:41:44 +0100

    ajouter class etudiant

---
+++ Etudiant.java
@@ -0,0 +1,42 @@
+public class Etudiant {
+    private String nom;
+    private int age;
+    private String matricule;
+
+    // Constructor
+    public Etudiant(String nom, int age, String matricule) {
+        this.nom = nom;
+        this.age = age;
+        this.matricule = matricule;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getMatricule() {
+        return matricule;
+    }
+
+    public void setMatricule(String matricule) {
+        this.matricule = matricule;
+    }
+
+    public void afficherInfo() {
+        System.out.println("Nom: " + nom);
+        System.out.println("Age: " + age);
+        System.out.println("Matricule: " + matricule);
+    }
+}
